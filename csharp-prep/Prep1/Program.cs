732763a33157d15e74094fe50508a9bad477d2ca
Jsime34 <jaimegut3005@gmail.com>
2024-06-11 15:14:27 -0600

    Added programs for prep 1 and 2 of the first week.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("Insert your first name here: ");
+        string f_name = Console.ReadLine();
+        Console.Write("Insert your last name here: ");
+        string l_name = Console.ReadLine();
+        Console.WriteLine($"Your name is {l_name}, {f_name} {l_name}");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Insert your grade number here: ");
+        string grade_t = Console.ReadLine();
+        int grade = int.Parse(grade_t);
+        string passed = "failed";
+        string letter = "F";
+        if(grade<60){
+            letter = "F";
+        }
+        else if(grade>=60 && grade<70){
+            letter = "D";
+            letter = changeSign(grade, letter);
+        }
+        else if(grade>=70 && grade<80){
+            letter = "C";
+            passed = "passed";
+            letter = changeSign(grade, letter);
+        }
+        else if(grade>=80 && grade<90){
+            letter = "B";
+            passed = "passed";
+            letter = changeSign(grade, letter);
+        }
+        else if(grade>=90){
+            letter = "A";
+            passed = "passed";
+            letter = changeSign(grade, letter);
+        }
+        if(passed == "passed"){
+            Console.Write($"Your grade letter is {letter} and you passed the class, congratulations!");
+        }
+        else{
+            Console.Write($"Your grade letter is {letter} and sadly you failed the class, better luck next time.");
+        }
+    }
+
+    static string changeSign(int grade, string letter){
+        if(grade%10 >= 7){
+            letter = letter + "+";
+        }
+        else if(grade%10 <7 && grade%10>3){
+            letter = letter;
+        }
+        else{
+            letter = letter + "-";
+        }
+        return letter;
     }
 }
