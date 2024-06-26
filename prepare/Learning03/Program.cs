2c5165cf3c68a4c810808e2fbaf6ff50a65e411a
Jsime34 <jaimegut3005@gmail.com>
2024-06-26 13:23:37 -0600

    Learning 03 fraction assignment

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f1 = new Fraction();
+        Console.Write($"{f1.GetFractionString()}\n");
+        Fraction f2 = new Fraction(5);
+        Console.Write($"{f2.GetFractionString()}\n");
+        Fraction f3 = new Fraction(3, 4);
+        Console.Write($"{f3.GetFractionString()}\n");
+        f1.SetTop(1);
+        f1.SetBottom(3);
+        Console.Write($"{f1.GetBottom()}\n");
+        Console.Write($"{f1.GetTop()}\n");
+        Console.Write($"{f1.GetFractionString()}\n");
+        Console.Write($"{f1.GetDecimalValue()}\n");
     }
 }
